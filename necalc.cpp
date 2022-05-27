e5016bde01e43afa44d4b83adb39ff8c3eee1d16
Joshua Saxby <joshua.a.saxby@gmail.com>
2022-05-27 16:00:15 +0100

    Basic sample of reading input in line-by-line

--- necalc.cpp
+++ necalc.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <string>
+
+#include <arby/Nat.hpp>
+
+
+using namespace com::saxbophone;
 
 int main() {
     std::cout << "necalc v" << NECALC_VERSION_STRING << std::endl;
+    arby::Nat accumulator;
+    std::string input;
+    do {
+        if (not input.empty()) {
+            std::cout << input << std::endl;
+        }
+        std::cout << "> ";
+    } while (std::getline(std::cin, input));
+    std::cout << std::endl; // print newline on escape/exit
 }
